54dfef518e7b95d3e06aa7f71d242e5dcf2867db
unknown <nikola.miholjcic@hotmail.com>
2024-10-12 21:30:29 +0200

    initial

--- src/components/Input/InputText/index.jsx
+++ src/components/Input/InputText/index.jsx
@@ -1,6 +1,8 @@
 import styles from "./InputText.module.scss";
-import TooltipArrow from "../../../assets/svgs/TooltipArrow";
-
+import TooltipArrow from "./svgs/TooltipArrow";
+import Eye from "./svgs/Eye";
+import EyeShut from "./svgs/EyeShut";
+import { useState } from "react";
 export default function InputText({
   type = "text",
   isPassword = false,
@@ -13,56 +15,72 @@ export default function InputText({
   onFocus = () => {},
   value = "",
   varName = "",
+  toggable = false,
 }) {
-  if (isPassword) {
+  const [isShowingPassword, setIsShowingPassword] = useState(isPassword);
+  if (isShowingPassword) {
     value = "*".repeat(value.length);
   }
 
   return (
-    <div className="d-flex flex-column container position-relative ">
+    <>
       {label !== "" && <span className={styles.label}>{label}</span>}
-      {type === "text" ? (
-        <input
-          placeholder={`${placeholder}`}
-          type="text"
-          readOnly={readOnly}
-          value={value}
-          className={`${styles.inputText} ${styles.text} ${
-            invalid ? styles.invalid : ""
-          }`}
-          onChange={(event) => {
-            onChange(event, varName);
-          }}
-          onFocus={() => {
-            onFocus(varName);
-          }}
-        />
-      ) : (
-        <textarea
-          onChange={(event) => {
-            onChange(event, varName);
-          }}
-          onFocus={() => {
-            onFocus(varName);
-          }}
-          placeholder={`${placeholder}`}
-          readOnly={readOnly}
-          value={value}
-          className={`${styles.inputText} ${styles.textArea}`}
-        />
-      )}
-      {invalid && (
-        <>
-          <span className={`${styles.errorX}`}>x</span>
-          <span
-            className={`${styles.errorMessage} + " d-flex flex-row justify-content-center align-items-center no-wrap"`}
+      <div className="d-flex flex-column container position-relative ">
+        {type === "text" ? (
+          <input
+            placeholder={`${placeholder}`}
+            type="text"
+            readOnly={readOnly}
+            value={value}
+            className={`${styles.inputText} ${styles.text} ${
+              invalid ? styles.invalid : ""
+            }`}
+            onChange={(event) => {
+              onChange(event, varName);
+            }}
+            onFocus={() => {
+              onFocus(varName);
+            }}
+          />
+        ) : (
+          <textarea
+            onChange={(event) => {
+              onChange(event, varName);
+            }}
+            onFocus={() => {
+              onFocus(varName);
+            }}
+            placeholder={`${placeholder}`}
+            readOnly={readOnly}
+            value={value}
+            className={`${styles.inputText} ${styles.textArea}`}
+          />
+        )}
+        {invalid && (
+          <>
+            <span className={`${styles.errorX}`}>x</span>
+            <span
+              className={`${styles.errorMessage} + " d-flex flex-row justify-content-center align-items-center no-wrap"`}
+            >
+              {" "}
+              <TooltipArrow /> Please enter Your{" "}
+              {errorMessasge !== "" ? errorMessasge : placeholder.toLowerCase()}
+            </span>
+          </>
+        )}
+        {!invalid && toggable && (
+          <div
+            className={styles.visibilityToggle}
+            onClick={() => {
+              setIsShowingPassword((oldState) => !oldState);
+              console.log("some");
+            }}
           >
-            {" "}
-            <TooltipArrow /> Please enter Your{" "}
-            {errorMessasge !== "" ? errorMessasge : placeholder.toLowerCase()}
-          </span>
-        </>
-      )}
-    </div>
+            {!isShowingPassword && <EyeShut />}
+            {isShowingPassword && <Eye />}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
--- src/components/pages/ChangePassword/index.jsx
+++ src/components/pages/ChangePassword/index.jsx
@@ -1,4 +1,24 @@
 import styles from "./ChangePassword.module.scss";
+import InputText from "../../Input/InputText";
+import Button from "../../Button";
 export default function ChangePassword() {
-  return <div className={`${styles.changePassword}`}></div>;
+  return (
+    <div className={`${styles.changePassword}`}>
+      <h3>Change Password</h3>
+      <div className="row">
+        <div className="col-md-6 col-12 col">
+          <InputText isPassword label="Old Password" toggable />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 col-12 col">
+          <InputText isPassword label="New Password" />
+        </div>
+        <div className="col-md-6 col-12 col">
+          <InputText label="Retype New Pasword" />
+        </div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 }
